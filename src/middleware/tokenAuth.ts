d65760b9e21f1320a40f9d2743671438a00b6ec3
Prashanith <prashanith007@gmail.com>
2025-06-29 16:27:23 +0530

    authfix

--- src/middleware/tokenAuth.ts
+++ src/middleware/tokenAuth.ts
@@ -1,18 +1,21 @@
 import jwt from "jsonwebtoken";
-import { HttpResponse } from "../models/http/response";
+import { HttpResponse } from "../models/http/response.ts";
+import { NextFunction, Request, Response } from "express";
+import { secrets } from "../utils/envUtils.ts";
 
-function tokenAuth(request, response, next) {
+function tokenAuth(request: Request, response: Response, next: NextFunction) {
   try {
     const authToken = String(request.headers["authorization"]).split(" ")[1];
     if (authToken) {
       jwt.verify(
         authToken,
-        process.env.ACCESS_TOKEN_SECRET,
-        function (err, digest) {
+        secrets.ACCESS_TOKEN_SECRET,
+        function (err, digest: string | jwt.JwtPayload | undefined) {
           if (err) {
             return HttpResponse.toUnauthorizedError(response);
           } else {
-            request.body["context"] = { id: digest.id };
+            if (digest && typeof digest == "object")
+              request.body["context"] = { id: digest?.id ?? "" };
           }
         }
       );
